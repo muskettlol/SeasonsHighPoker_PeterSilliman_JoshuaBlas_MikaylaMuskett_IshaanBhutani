2956054b18c7ed8b84aa7ae3ac3f56cbe936b842
Peter Silliman <petersilliman03@gmail.com>
2022-02-16 22:48:40 -0800

    Initial commit

---
+++ app/src/main/java/com/gmail/seasonshighpoker/MainActivity.java
@@ -0,0 +1,18 @@
+package com.gmail.seasonshighpoker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import java.util.Hashtable;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
