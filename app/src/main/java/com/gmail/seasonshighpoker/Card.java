8834528460b660c7e14aa547304446ccb6577994
Peter Silliman <petersilliman03@gmail.com>
2022-02-21 20:24:48 -0800

    Created Card objects and deck list

--- app/src/main/java/com/gmail/seasonshighpoker/Card.java
+++ app/src/main/java/com/gmail/seasonshighpoker/Card.java
@@ -1,6 +1,20 @@
 package com.gmail.seasonshighpoker;
 
 public class Card {
-    int totalCardCount = 54;
+
+    //instantiate card value and suit
+    int cardValue;
+    String cardSuit;
+
+    //create a card
+    public Card(int value, String suit){
+        //declare value and suit
+        this.cardValue = value;
+        this.cardSuit = suit;
+    }
+
+    public int getValue(){ return this.cardValue; }
+
+    public String getSuit(){ return this.cardSuit; }
 
 }
--- app/src/main/java/com/gmail/seasonshighpoker/GoldenGameState.java
+++ app/src/main/java/com/gmail/seasonshighpoker/GoldenGameState.java
@@ -1,16 +1,35 @@
 package com.gmail.seasonshighpoker;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GoldenGameState {
 
-    //Instantiate variables and declare base values
+    /** Instantiate variables and declare base values*/
     int potBalance = 0;
     // need to create card object class "Card"
-    // ArrayList<Card> deck = new ArrayList<Card>();
-    // ArrayList<Card> discardPile = new ArrayList<Card>();
+    List<Card> deck = new ArrayList<Card>();
+    List<Card> discardPile = new ArrayList<Card>();
 
     public GoldenGameState(){
+        //Creates all 52 card objects and puts them into the deck arraylist
+        String suit = null;
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < 13; j++){
+                if(i == 0){
+                    suit = "Club";
+                } else if(i == 1){
+                    suit = "Heart";
+                } else if(i == 2){
+                    suit = "Diamond";
+                } else {
+                    suit = "Spade";
+                }
+                deck.add(new Card(j, suit));
+            }
+        }
+
 
     }
-
 
 }
